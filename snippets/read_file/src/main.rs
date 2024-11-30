35a676901e4573917b0503cf9662d2f8f7c2a707
SangersJeroen <jeroen98.sangers@gmail.com>
2024-11-30 20:41:44 +0100

    cleanup

--- snippets/read_file/src/main.rs
+++ snippets/read_file/src/main.rs
@@ -1,11 +1,14 @@
 // read txt file
 
-fn read_file(path_to_file: &str) -> String {
-    std::fs::read_to_string(path_to_file).expect("FileNotFoundError")
+fn read_file(path_to_file: &str) -> Result<String, std::io::Error> {
+    std::fs::read_to_string(path_to_file)
 }
 
 fn main() {
     let path_to_file = "input.txt";
-    let input: String = read_file(path_to_file);
-    println!("{}", input)
+    let input: String = match read_file(path_to_file) {
+        Ok(str) => str,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+    println!("{input}");
 }
--- snippets/read_file/target/debug/deps/read_file-7ebd50fc5d7b2056.d
+++
@@ -1,5 +0,0 @@
-/home/jeroensangers/Repos/advent_of_code/snippets/read_file/target/debug/deps/libread_file-7ebd50fc5d7b2056.rmeta: src/main.rs
-
-/home/jeroensangers/Repos/advent_of_code/snippets/read_file/target/debug/deps/read_file-7ebd50fc5d7b2056.d: src/main.rs
-
-src/main.rs:
