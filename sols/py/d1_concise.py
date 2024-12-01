e1398d086dea5da90aea438d7b6ac2a0b3e139f3
SangersJeroen <jeroen98.sangers@gmail.com>
2024-12-01 17:11:25 +0100

    day 1 p1/p2 python, concise

--- sols/py/d1_concise.py
+++ sols/py/d1_concise.py
@@ -18,21 +18,8 @@ if __name__ == "__main__":
 
     # Part 2
     list_left, list_right = parse_to_lists(file, cast=int)
-    counts: list[int] = list()
-
-    # Iterating over entries in the left list
-    # O(n**2)
-    for ll in list_left:
-        count: int = 0
-        # Check whether the right entry is the same as the current left
-        # entry for every entry in the right list, if so add one to the
-        # count
-        for rr in list_right:
-            if ll == rr:
-                count += 1
-        counts.append(count)
-
     # Take the sum of the similarity score per entry in the left list
     # which is the number of occurences in the right list times the 
     # the value in the left list
-    print(sum([ll * cc for ll, cc in zip(list_left, counts)]))
+    similarity = sum([sum([1 if ll == rr else 0 for rr in list_right])*ll for ll in list_left])
+    print(similarity)
